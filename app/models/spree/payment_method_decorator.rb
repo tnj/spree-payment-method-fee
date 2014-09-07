dca84c1b396bc198bd00976ea71ce3a555f856f5
James Hilliard <james.hilliard1@gmail.com>
2014-09-07 04:16:21 -0600

    change to attr_accessor

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,6 +1,6 @@
 Spree::PaymentMethod.class_eval do
   has_many :fees, class_name: 'Spree::PaymentMethodFee', foreign_key: 'payment_method_id'
-  attr_accessible :fees_attributes
+  attr_accessor :fees_attributes
 
   accepts_nested_attributes_for :fees, allow_destroy: true
 end
