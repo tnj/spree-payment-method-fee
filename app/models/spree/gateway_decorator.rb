e40a6a6471b366b514869d396d88b95251eb20bb
James Hilliard <james.hilliard1@gmail.com>
2014-09-07 04:14:43 -0600

    change to attr_accessor

--- app/models/spree/gateway_decorator.rb
+++ app/models/spree/gateway_decorator.rb
@@ -2,5 +2,5 @@ Spree::Gateway.class_eval do
   # Due some wierdness with STI, we need to declare this here.
   # Otherwise, we get "undefined method `association_class' for nil:NilClass"
   has_many :fees, class_name: 'Spree::PaymentMethodFee', foreign_key: 'payment_method_id'
-  attr_accessible :fees_attributes
+  attr_accessor :fees_attributes
 end
