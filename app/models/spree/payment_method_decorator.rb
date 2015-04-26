0846e95696d6d8713e6aa77875946617f184c3c2
tnj <kitakore@gmail.com>
2015-04-26 23:36:18 +0900

    fix saving attributes
    quick & dirty hack to get rid of strong parameters

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,6 +1,5 @@
 Spree::PaymentMethod.class_eval do
   has_many :fees, class_name: 'Spree::PaymentMethodFee', foreign_key: 'payment_method_id'
-  attr_accessor :fees_attributes
 
   accepts_nested_attributes_for :fees, allow_destroy: true
 end
